60d478765824ecc8d76ddcffda7cd6e43f1a0c2b
Bartłomiej Karczmarczyk <bartek.karczmarczyk@gmail.com>
2019-09-25 08:38:22 +0200

    Created Reusable Card component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { fetchPopularRepos } from '../utils/api';
 import { FaUser, FaStar, FaCodeBranch, FaExclamationTriangle } from 'react-icons/fa';
+import Card from './Card';
 
 function LanguagesNav ({ selected, onUpdateLanguage }) {
   const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
@@ -30,19 +31,14 @@ function ReposGrid ({ repos }) {
         const { login, avatar_url } = owner;
 
         return (
-          <li key={html_url} className='repo bg-light'>
-            <h4 className='header-lg center-text'>
-              #{index + 1}
-            </h4>
-            <img
-            className='avatar'
-            src={avatar_url}
-            alt={`Avatar for ${login}`}
-            />
-            <h2 className='cener-text'>
-              <a className='link' href={html_url}>{login}</a>
-            </h2>
-            <ul className='card-list'>
+          <li key={html_url} className='card bg-light'>
+            <Card
+              header={`#${index + 1}`}
+              avatar={avatar_url}
+              href={html_url}
+              name={login}
+            >
+              <ul className='card-list'>
               <li>
                 <FaUser color='rgb(255, 191, 116)' size={22}/>
                 <a href={`https://github.com/${login}`}>
@@ -61,7 +57,8 @@ function ReposGrid ({ repos }) {
                 <FaExclamationTriangle color='rgb(241, 138, 147)' size={22} />
                 {open_issues.toLocaleString()} open issues
               </li>
-            </ul>
+              </ul>
+            </Card>
           </li>
         )
       })}
